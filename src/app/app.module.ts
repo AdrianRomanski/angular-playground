3c516d12f538e1f7fb0a723d685a04abccaeb11e
Adrian Romański <muszynianka1337@gmail.com>
2023-07-11 19:46:07 +0200

    websites - angular - developer guides - testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+    DeveloperGuidesTestingModule
+} from "./websites/angular/developer_guides/testing/app/developer-guides-testing.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        DeveloperGuidesTestingModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/websites/angular/developer_guides/testing/app/developer-guides-testing.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'developer-guides-testing',
+  templateUrl: './developer-guides-testing.component.html'
+})
+export class DeveloperGuidesTestingComponent { }
